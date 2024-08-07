f409ebccfe824a3d533107d0131bb92616737df8
ryan0426 <1141653364@qq.com>
2024-08-06 17:20:47 -0700

    modify setting

--- web/settings.py
+++ web/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#mj_dz-03k)a$k*g1ugu*@kxa^p=tpcvw=@gs%v@j#)pp9yet7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['81.70.26.42']
 
 
 # Application definition
