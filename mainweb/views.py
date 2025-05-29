f31fb5846013c23b8a93ae9d856ece9790c91571
Rujun Yan <1141653364@qq.com>
2025-05-28 17:08:01 -0700

    change header and detail

--- mainweb/views.py
+++ mainweb/views.py
@@ -14,38 +14,8 @@ def tech_info(request):
 def news(request):
     return render(request, 'news.html')
 
-def dies_view(request):
-    return render(request, 'categories/dies.html')
-
-def wear_parts_view(request):
-    return render(request, 'categories/wear_parts.html')
-
-def heading_dies_view(request):
-    return render(request, 'categories/heading_dies.html')
-
-def bit_view(request):
-    return render(request, 'categories/bit.html')
-
-def rod_view(request):
-    return render(request, 'categories/rod.html')
-
-def cutting_tool_view(request):
-    return render(request, 'categories/cutting_tool.html')
-
-def cutter_view(request):
-    return render(request, 'categories/cutter.html')
-
-def ram_pot_view(request):
-    return render(request, 'categories/ram_pot.html')
-
-def non_magnetic_view(request):
-    return render(request, 'categories/non_magnetic.html')
-
-def precision_mold_view(request):
-    return render(request, 'categories/precision_mold.html')
-
-def other_tools_view(request):
-    return render(request, 'categories/other_tools.html')
+def testing_equip_view(request):
+    return render(request, 'testing_equip.html')
 
 def precision_view(request):
     return render(request, 'categories/precision.html')
@@ -61,6 +31,3 @@ def mining_view(request):
 
 def rollers_view(request):
     return render(request, 'categories/rollers.html')
-
-def testing_equip_view(request):
-    return render(request, 'testing_equip.html')
